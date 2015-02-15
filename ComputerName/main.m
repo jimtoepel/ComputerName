24fb7556d4bb26db627bcf05bb6dcb6f5c0f25bd
jimtoepel <jim.toepel@gmail.com>
2015-02-15 14:08:26 -0800

    I CAN'T BELIEVE THAT WORKED.

--- ComputerName/main.m
+++ ComputerName/main.m
@@ -10,8 +10,20 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+    // create an instance of NSHost that has LapToep's info
+        // send currentHost message to NSHost
+        NSHost *compy =[NSHost currentHost];
+
+        
+    // ask that instance for LapToep's name
+        // localizedName message returns a pointer to SSString -> NSString*
+        NSString *compyName = [compy localizedName];
+        
+        
+    //  Log it with NSLog()
+        // use the %@ token to print out the name.
+        NSLog(@"\nWelcome to %@, user", compyName);
     }
     return 0;
 }
